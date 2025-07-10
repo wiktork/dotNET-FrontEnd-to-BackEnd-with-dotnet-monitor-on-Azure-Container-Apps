1defcd16e2c722e482652b5870e79a7ca849754c
Wiktor Kopec <wiktork@microsoft.com>
2025-07-09 18:22:54 -0700

    Build

--- BankingApp/AppHost/Program.cs
+++ BankingApp/AppHost/Program.cs
@@ -2,12 +2,14 @@ using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
+var parameter = builder.AddParameter("pat", pat, secret: true);
 
 var apiService = builder.AddDockerfile("bankservice", "..", "Banking.AccountApi/Dockerfile", "final");
 apiService.WithEndpoint(targetPort: 8080, scheme: "http");
 
 var uiBuilder = builder.AddDockerfile("bankui", "..", "Banking.WebUI/Dockerfile", "final");
     uiBuilder.WithEndpoint(targetPort: 8080, scheme: "http", isProxied: true, isExternal: true)
+    .WithBuildSecret("FEED_PAT", parameter)
     .WithReference(apiService.GetEndpoint("bankservice"))
     .WaitFor(apiService);
 
