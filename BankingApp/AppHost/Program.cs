4d43318bb458aa075b4145e656da9c146dc2fd4a
Wiktor Kopec <wiktork@microsoft.com>
2025-07-10 09:30:43 -0700

    Switch to regular arg

--- BankingApp/AppHost/Program.cs
+++ BankingApp/AppHost/Program.cs
@@ -2,14 +2,15 @@ using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
-var parameter = builder.AddParameter("pat", pat, secret: true);
+string pat = @"";
+var parameter = builder.AddParameter("pat", pat, secret: false);
 
 var apiService = builder.AddDockerfile("bankservice", "..", "Banking.AccountApi/Dockerfile", "final");
 apiService.WithEndpoint(targetPort: 8080, scheme: "http");
 
 var uiBuilder = builder.AddDockerfile("bankui", "..", "Banking.WebUI/Dockerfile", "final");
     uiBuilder.WithEndpoint(targetPort: 8080, scheme: "http", isProxied: true, isExternal: true)
-    .WithBuildSecret("FEED_PAT", parameter)
+    .WithBuildArg("FEED_PAT", parameter)
     .WithReference(apiService.GetEndpoint("bankservice"))
     .WaitFor(apiService);
 
