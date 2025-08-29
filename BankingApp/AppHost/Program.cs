350d04c3c5ee616e2109d2e310b5f9c90b7921c2
Wiktor Kopec <wiktork@microsoft.com>
2025-08-29 11:47:18 -0700

    Remove cli

--- BankingApp/AppHost/Program.cs
+++ BankingApp/AppHost/Program.cs
@@ -7,14 +7,14 @@ using Projects;
 // modified procdump build
 
 var builder = DistributedApplication.CreateBuilder(args);
-var parameter = builder.AddParameter("pat", value:builder.Configuration["pat"]);
+//var parameter = builder.AddParameter("pat", value:builder.Configuration["pat"]);
 
 var apiService = builder.AddDockerfile("bankservice", "..", "Banking.AccountApi/Dockerfile", "final");
 apiService.WithEndpoint(targetPort: 8080, scheme: "http");
 
 var uiBuilder = builder.AddDockerfile("bankui", "..", "Banking.WebUI/Dockerfile", "final");
     uiBuilder.WithEndpoint(targetPort: 8080, scheme: "http", isProxied: true, isExternal: true)
-    .WithBuildArg("FEED_PAT", parameter)
+    //.WithBuildArg("FEED_PAT", parameter)
     .WithReference(apiService.GetEndpoint("bankservice"))
     .WaitFor(apiService);
 
